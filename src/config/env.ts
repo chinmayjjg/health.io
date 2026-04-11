d2cd1c68f3e1ce8fb01ab3659f9ba98013fb5038
chinmay pradhan <147341405+chinmayjjg@users.noreply.github.com>
2026-04-11 12:15:49 +0530

    razor pay intigarting

--- src/config/env.ts
+++ src/config/env.ts
@@ -14,4 +14,6 @@ export const env = {
   PORT: Number(process.env.PORT) || 5000,
   MONGO_URI: process.env.MONGO_URI as string,
   JWT_SECRET: process.env.JWT_SECRET as string,
+  RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID || "",
+  RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET || "",
 };
